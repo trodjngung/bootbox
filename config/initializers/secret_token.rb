1c3b0eb283ed28f1077b36a62365b907b366ce75
trodjngung <ngoduytrung2901@gmail.com>
2013-08-13 09:44:32 +0700

    bootbox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootbox::Application.config.secret_key_base = '7436b285ebf842959f2c1058b29d113f4f461beb2b287b917d1bb669d42c4f3a7a0f21138a09eb2f8ac7708f8d7678613baf42d5d1b979d40d50a44feedb0a31'
